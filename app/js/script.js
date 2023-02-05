ebb6eae419313a28a534a2bdd11ee441bb80d00c
Thiago <thiagocordeirodantas0@Gmail.com>
2023-02-05 03:18:35 -0300

    Desafio concluido Mentalita

--- app/js/script.js
+++ app/js/script.js
@@ -1,25 +1,29 @@
-const numeroAleatorio = parseInt(Math.random(0) * 101); 
-const numeroDeTentativas = 0;  
+var numeroAleatorio = parseInt(Math.random(0) * 101); 
+var numeroDeTentativas =0;  
 
    
     while(tenteAcertar != numeroAleatorio){
+        
+       if (numeroDeTentativas != 0){ 
+        alert(" Voce ja tentou  " + numeroDeTentativas + " vezes " )
+        }   
 
         var tenteAcertar = prompt("Digite um Numero")
 
         if(tenteAcertar == numeroAleatorio){
             alert(`Voce Acertou!!! O numero secreto era ${numeroAleatorio}`)
-            numeroDeTentativas += 1;
+            tenteAcertar
         } else if (tenteAcertar > numeroAleatorio){
             alert("Otimo chute.. mas esse numero e maior que o secreto ")
         } else if (tenteAcertar < numeroAleatorio){
             alert("Muito triste... mas esse numero e menor que o secreto")
 
-        } else if (numeroDeTentativas != 0){ 
-            alert(`Voce ja tentou ${numeroDeTentativas}`)
-        }   
+        } 
+        numeroDeTentativas += 1
     }
+ 
 
-    numeroDeTentativas += 1;
+   
      
 
 
