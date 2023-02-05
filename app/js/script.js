f7f440785e2ac1ace2f7f272d33bf8c169c85062
Thiago <thiagocordeirodantas0@Gmail.com>
2023-02-05 03:05:17 -0300

    Desafio mentalista concluido

--- app/js/script.js
+++ app/js/script.js
@@ -1,24 +1,26 @@
+const numeroAleatorio = parseInt(Math.random(0) * 101); 
+const numeroDeTentativas = 0;  
 
+   
+    while(tenteAcertar != numeroAleatorio){
 
-function secreto(){
-    let numeroSecreto = parseInt( Math.random(0) * 100 )
-    let chute = prompt('Digite um numero entre 0 e 1000')
+        var tenteAcertar = prompt("Digite um Numero")
 
-    while (chute != numeroSecreto) { 
-        // se o chute for igual ao numero secreto
-        if(chute == numeroSecreto ){
-            alert(` Parabens!!!! o numero secreto era ${numeroSecreto}`)
-        } else if(chute  > numeroSecreto){
-            alert(' Errou... o Numero e Menor')
-        } else if ( chute < numeroSecreto){
-            alert('Errou... o Numero e Maior')
-        }
-        
+        if(tenteAcertar == numeroAleatorio){
+            alert(`Voce Acertou!!! O numero secreto era ${numeroAleatorio}`)
+            numeroDeTentativas += 1;
+        } else if (tenteAcertar > numeroAleatorio){
+            alert("Otimo chute.. mas esse numero e maior que o secreto ")
+        } else if (tenteAcertar < numeroAleatorio){
+            alert("Muito triste... mas esse numero e menor que o secreto")
 
+        } else if (numeroDeTentativas != 0){ 
+            alert(`Voce ja tentou ${numeroDeTentativas}`)
+        }   
     }
-}
+
+    numeroDeTentativas += 1;
+     
 
 
-secreto();
-
-
+    
